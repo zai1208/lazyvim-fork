0cd5310f8928c64519ec8e865988e405fc85c0a6
zai1208 <zai1208@outlook.com>
2025-10-05 18:43:05 +1100

    added mdmath.nvim & replaced snippets with other snippets

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -1,1 +1,9 @@
-return { "sidonthetroll/friendly-snippets-math", lazy = false }
+return {
+  "iurimateus/luasnip-latex-snippets.nvim",
+  -- vimtex isn't required if using treesitter
+  requires = { "L3MON4D3/LuaSnip" },
+  config = function()
+    require("luasnip-latex-snippets").setup({ use_treesitter = true })
+    require("luasnip").config.setup({ enable_autosnippets = true })
+  end,
+}
