b6824bb6401d3a601f23188e4fe892c6e21a9949
Zain Khan <contact@zain-khan.dev>
2026-06-01 08:25:22 +1000

    idk

--- lua/plugins/main.lua
+++ lua/plugins/main.lua
@@ -6,9 +6,30 @@ return {
     },
   },
   {
+    "oribarilan/folio.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("folio").setup()
+    end,
+  },
+  {
     "alex-popov-tech/store.nvim",
     dependencies = { "OXY2DEV/markview.nvim" },
     opts = {},
     cmd = "Store",
+  },
+  {
+    "brianhuster/live-preview.nvim",
+    dependencies = {
+      -- You can choose one of the following pickers
+      "folke/snacks.nvim",
+    },
+    opts = {
+      dyanmic_root = true,
+    },
+  },
+  {
+    "alaviss/nim.nvim",
   },
 }
